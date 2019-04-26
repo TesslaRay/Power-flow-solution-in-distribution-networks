966fcf05332719ccbd5b9ab28077ca52578eed8d
TesslaRay <cristian@MacBook-Pro-de-Cristian.local>
2019-04-26 17:46:34 -0400

    Modificación de la secuencia de ajuste

--- Backward.m
+++ Backward.m
@@ -17,7 +17,7 @@ linedata =[
 loaddata =[ 1       0         0  	  0
             2       100       60 	  0
             3       90        40      0
-            4       120       80	  0
+            4       120       80	  0            
           ];
 
 % P.U
@@ -34,7 +34,7 @@ branch = [4,3,2,1];
 Vb_k = ones(4,1);
 Vb_k1 = ones(4,1);
 
-e = 1E-6;
+e = 1E-8;                   % tolerancia
 
 Ish(4) = complex(1,0);
 
@@ -42,7 +42,7 @@ for IT = 1:10
     
     Zsh = conj(Vb_k.^2./S);
     
-    for i = 1:length(branch)
+    for i = 1:length(branch) - 1
 %         branch(i)
         if branch(i) == 4
 %             disp('nodo terminal')
@@ -51,42 +51,6 @@ for IT = 1:10
 
             Vb_k(branch(i)) = Ish(branch(i))*Zsh(branch(i));
 
-        elseif branch(i) == 1
-%             disp('ajuste')
-
-            Vb_k(branch(i)) = Vb_k(branch(i - 1)) + Z(branch(i))*Ibr(branch(i));
-
-            % how do I adjust the node voltage and the branch current?
-
-            Ish(4) = complex(1,0)*Ish(4)/Vb_k(1);
-            
-            for x = 1:length(branch)
-%                 branch(x)
-                
-                if branch(x) == 4
-%                     disp('ajuste nodo terminal')
-
-                    Vb_k1(branch(x),1) = Zsh(branch(x))*Ish(branch(x));
-
-                    Ibr(branch(x) - 1,1) = Ish(branch(x));
-
-                elseif branch(x) == 1
-%                     disp('ajuste nodo inicial')
-
-                    Vb_k1(branch(x)) = Vb_k1(branch(x - 1)) + Z(branch(x))*Ibr(branch(x));
-               
-                else
-%                     disp('ajuste nodo normal')
-
-                    Vb_k1(branch(x)) = Vb_k1(branch(x - 1)) + Z(branch(x))*Ibr(branch(x));
-
-                    Ish(branch(x)) = Vb_k1(branch(x))/Zsh(branch(x));
-
-                    Ibr(branch(x) - 1,1) = Ish(branch(x)) + Ibr(branch(x));
-                    
-                end
-            end
-            
         else
 %             disp('nodo normal')
 
@@ -99,6 +63,40 @@ for IT = 1:10
         end
     end
     
+%     disp('Ajuste')
+    Vb_k(1) = Vb_k(2) + Z(1)*Ibr(1);
+
+%     how do I adjust the node voltage and the branch current?
+
+    Ish(4) = complex(1,0)*Ish(4)/Vb_k(1);
+            
+    for x = 1:length(branch)
+%         branch(x)
+                
+        if branch(x) == 4
+%             disp('ajuste nodo terminal')
+                    
+            Vb_k1(branch(x),1) = Zsh(branch(x))*Ish(branch(x));
+                    
+            Ibr(branch(x) - 1,1) = Ish(branch(x));
+   
+        elseif branch(x) == 1
+%             disp('ajuste nodo inicial')
+                    
+        Vb_k1(branch(x)) = Vb_k1(branch(x - 1)) + Z(branch(x))*Ibr(branch(x));
+                    
+        else
+%             disp('ajuste nodo normal')
+                    
+            Vb_k1(branch(x)) = Vb_k1(branch(x - 1)) + Z(branch(x))*Ibr(branch(x));
+                    
+            Ish(branch(x)) = Vb_k1(branch(x))/Zsh(branch(x));
+                    
+            Ibr(branch(x) - 1,1) = Ish(branch(x)) + Ibr(branch(x));
+                    
+        end
+    end
+       
     if abs(Vb_k - Vb_k1) < e
         break
     end
